a0286571901d6806388a6c495b85726729e12331
kev-n14 <kevinfinn688@gmail.com>
2022-10-26 15:07:16 +0000

    add '

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
-var pos = 0,
-    buttons, test, test_status, question, choice, choices, chA, chB, chC, chD, correct = 0;
-// const myImage = new Image(100, 600);
-// // myImage.src = 'assets/images/question1.jpg';
-// document.body.appendChild(myImage);
+const nextBtn = document.getElementById('next');
+const answers = document.getElementsByClassName("answers");
 
+// const answerHistory = [];
+var pos = 0,
+    buttons, questionArea, content, question, choice, choices, chA, chB, chC, chD, correct = 0;
 
 var questions = [{ //1
     question: "How many Infinity Stones are there?",
@@ -12,7 +12,7 @@ var questions = [{ //1
     c: "4",
     d: "9",
     answer: "B",
-    //Add property to hold image source uri, either local or online
+
     img: "assets/images/question1.jpg"
 },
 { //1
@@ -22,7 +22,7 @@ var questions = [{ //1
     c: "Clare",
     d: "New York",
     answer: "A",
-    //Add property to hold image source uri, either local or online
+
     img: "assets/images/question2.jpg"
 },
 { //1
@@ -32,66 +32,103 @@ var questions = [{ //1
     c: "Tom Ryan",
     d: "Howard Stark",
     answer: "D",
-    //Add property to hold image source uri, either local or online
+
     img: "assets/images/question3.jpg"
 }];
+
+
 
 function get(x) {
     return document.getElementById(x);
 }
 
 function renderQuestion() {
-    buttons = get("buttons");
-    test = get("test");
+
+    questionArea = get("questionArea");
+
     if (pos >= questions.length) {
-        test.innerHTML = "<h2>You got " + correct + " of " + questions.length + " questions correct</h2>";
-        get("test_status").innerHTML = "Test completed";
+        questionArea.innerHTML = "<h2>You got " + correct + " of " + questions.length + " questions correct</h2>";
+        get("content").innerHTML = "questionArea completed";
 
         pos = 0;
         correct = 0;
 
         return false;
     }
-    // get("test_status").innerHTML = "Question " + (pos + 1) + " of " + questions.length;
 
     question = questions[pos].question;
     chA = questions[pos].a;
     chB = questions[pos].b;
     chC = questions[pos].c;
     chD = questions[pos].d;
-    //Add local var to hold uri
     img = questions[pos].img;
-    get("test_status").innerHTML = `<h3>Question ${pos + 1} of ${questions.length}</h3>`;
-    test.innerHTML = "<h3>" + question + "</h3>";
 
-    //Add <img> element to DOM with source
-    test.innerHTML += "<img src=\"" + img + "\" width=\"200\" height=\"200\"><br>";
+    get("content").innerHTML = `<h3>Question ${pos + 1} of ${questions.length}</h3>`;
+    questionArea.innerHTML = "<h3>" + question + "</h3>";
 
-    test.innerHTML += "<div class='choices'><label class='answers'> <input type='radio' name='choices' value='A'> " + chA + "</label>" +
-        "<label class='answers'> <input type='radio' name='choices' value='B'> " + chB + "</label>" +
+    questionArea.innerHTML += "<img src=\"" + img + "\" width=\"200\" height=\"200\"><br>";
+
+    questionArea.innerHTML += "<div class='choices'><label class='answers'> <input type='radio' name='choices' value='A'> " + chA + "</label>" +
+        "<label class='answers'> <input type='radio' name='choices' value='B' id='b'> " + chB + "</label>" +
         "<label class='answers'> <input type='radio' name='choices' value='C'> " + chC + "</label>" +
         "<label class='answers'> <input type='radio' name='choices' value='D'> " + chD + "</label></div>";
 
-    test.innerHTML += "<div class='buttons'><button onclick='' class='btn-grid'>Prev</button>" +
-        "<button onclick=''class='btn-grid'>Next</button>" +
-        "<button onclick='checkAnswer()'class='btn-grid'>Submit Answer</button></div>";
+    questionArea.innerHTML += "<div class='buttons'><button onclick='restartBtn()' class='btn-grid' id='restartBtn'>Restart</button>" +
+        "<button onclick='prev()' class='btn-grid'>Prev</button>" +
+        "<button onclick='next()' class='btn-grid'>Next</button>" +
+        "<button onclick='checkAnswer()'class='btn-grid'>Submit</button></div>";
 
 }
 
 function checkAnswer() {
     choices = document.getElementsByName("choices");
+    // const answerHistory = [];
+
     for (var i = 0; i < choices.length; i++) {
         if (choices[i].checked) {
             choice = choices[i].value;
+
+
         }
     }
 
     if (choice == questions[pos].answer) {
         correct++;
+
     }
+    pos++;
+
+
+    renderQuestion();
+}
+function next() {
 
     pos++;
 
+    renderQuestion();
+}
+
+function prev() {
+
+    if (pos >= 1) {
+
+        pos--;
+        answerHistory[pos] = choices;
+
+    }
+
+
+    renderQuestion();
+}
+function restartBtn() {
+
+    pos = 0,
+        buttons, questionArea, content, question, choices, chA, chB, chC, chD, correct = 0;
+
+
+    for (var i = 0; i < choices.length; i++) {
+        choice = 0;
+    }
     renderQuestion();
 }
 
